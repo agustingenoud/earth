7f1c96c967753616cac0c7a418118a58d8c95301
agustingenoud <agustingenoud@gmail.com>
2021-11-17 16:24:08 -0300

    docker-compose spark>M+1+2 + jupyter

---
+++ control-env.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function token {
+  echo 'Your TOKEN for Jupyter Notebook is:'
+  SERVER=$(docker exec -it jupyter jupyter notebook list)
+  echo "${SERVER}" | grep '/notebook' | sed -E 's/^.*=([a-z0-9]+).*$/\1/'
+}
+
+case $1 in
+  token )
+  token
+    ;;
+
+  * )
+  printf "ERROR: Missing command\n  Usage: `basename $0` (start|stop|cleanup|token|logs|update|superset-start|superset-stop|superset-init)\n"
+  exit 1
+    ;;
+esac
